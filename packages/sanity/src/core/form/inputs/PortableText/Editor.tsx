ba39a6f1f8d2d6dc609691a2fddb7aa028f09aa3
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2023-11-20 13:25:37 +0100

    feat(portable-text-editor): support for range decorators
    This will add support for decorating selections inside the Portable Text Editor with custom components.
    This can be used for search highlighting, validation etc.

--- packages/sanity/src/core/form/inputs/PortableText/Editor.tsx
+++ packages/sanity/src/core/form/inputs/PortableText/Editor.tsx
@@ -4,6 +4,7 @@ import {
   type HotkeyOptions,
   type OnCopyFn,
   type OnPasteFn,
+  type RangeDecoration,
   type RenderAnnotationFunction,
   type RenderBlockFunction,
   type RenderChildFunction,
@@ -44,6 +45,7 @@ interface EditorProps {
   onToggleFullscreen: () => void
   path: Path
   readOnly?: boolean
+  rangeDecorations?: RangeDecoration[]
   renderAnnotation: RenderAnnotationFunction
   renderBlock: RenderBlockFunction
   renderChild: RenderChildFunction
@@ -80,6 +82,7 @@ export function Editor(props: EditorProps) {
     onToggleFullscreen,
     path,
     readOnly,
+    rangeDecorations,
     renderAnnotation,
     renderBlock,
     renderChild,
@@ -127,6 +130,7 @@ export function Editor(props: EditorProps) {
         onCopy={onCopy}
         onPaste={onPaste}
         ref={editableRef}
+        rangeDecorations={rangeDecorations}
         renderAnnotation={renderAnnotation}
         renderBlock={renderBlock}
         renderChild={renderChild}
@@ -146,6 +150,7 @@ export function Editor(props: EditorProps) {
       initialSelection,
       onCopy,
       onPaste,
+      rangeDecorations,
       renderAnnotation,
       renderBlock,
       renderChild,
